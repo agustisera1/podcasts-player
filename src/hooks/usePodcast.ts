7a68804b86d18ea5b2efccf70f400fceb1de8400
Agustín Tisera <agustisera1@gmail.com>
2023-07-08 13:24:38 -0300

    mark edge case (won't do)

--- src/hooks/usePodcast.ts
+++ src/hooks/usePodcast.ts
@@ -3,6 +3,10 @@ import { useStorageData } from "./useStorageData";
 
 export const usePodcast = () => {
   const { pid } = useParams();
+  /*  Edge case: The user arrives podcast/:id without having visited the main view
+      (a shared link for instance), so storage is empty. That case we should fetch the podcast
+      if (!storagePodcasts) ... 
+   */
   const { podcasts: storagePodcasts } = useStorageData();
   return { podcast: storagePodcasts?.find(({ id }) => id === pid) ?? null };
 };
